097afa41ae372c9d64b10b21237800452d6fabcf
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2022-02-15 12:59:14 -0500

    commit tree f805e26e922e2d9f1c930011e1a638cf8f0b2516

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+
   def edit
     @post = Post.find(params[:id])
   end
